ee433dd94c34b5898b86f124bd253abba0681351
HajAmir011 <amirmohammadkamali6@gmail.com>
2024-11-10 15:43:43 +0330

    create Login page

--- src/pages/FilimoIrancell/FilimoIrancell.js
+++ src/pages/FilimoIrancell/FilimoIrancell.js
@@ -1,5 +1,5 @@
 import Menu from "../../Menu/Menu";
-import Header from "../../Header/Header";
+import Header from "./Header/Header";
 import Product from "./Product/Product";
 import Footer from "../../Footer/Footer";
 const FilimoIrancell = () => {
